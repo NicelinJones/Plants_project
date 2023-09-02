b26e6d5a9f362b3ec9ccf5a5eac3f8c6369e0a1d
NicelinJones <139481866+NicelinJones@users.noreply.github.com>
2023-09-02 04:07:41 -0700

    Add files via upload

---
+++ check.js
@@ -0,0 +1,17 @@
+const rt=document.querySelector("body" );
+//console.log(rt);
+//rt.onclick =function(){
+  //  alert('hai');
+//}
+//rt.setAttribute("onclick",'alert ("hai")');
+//function rt1(){
+    //alert('hello');
+//};
+//rt.onclick=rt1;
+rt.addEventListener("click",callalert);
+function callalert(){
+    alert('hai viewers');
+    
+};
+//rt.removeEventListener("click",callalert);
+
